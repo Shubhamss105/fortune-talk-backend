be58535a7dbf79f8c02f42a58f2e76529fe4a809
Shubhamss105 <shubhamsingh.ss.1407@gmail.com>
2024-05-08 13:37:30 +0530

    web controller

--- controllers/webController.js
+++ controllers/webController.js
@@ -1,5 +1,5 @@
-const Blogs = require('../models/adminModel/Blogs');
-const Testimonials = require('../models/adminModel/Testimonial');
+const Blogs = require("../models/adminModel/Blogs");
+const Testimonials = require("../models/adminModel/Testimonial");
 
 //blogs
 exports.getAllBlogs = async (req, res) => {
@@ -7,28 +7,31 @@ exports.getAllBlogs = async (req, res) => {
     const allBlogs = await Blogs.find();
 
     if (allBlogs.length === 0) {
-      return res.status(404).json({ success: false, message: 'No blogs found' });
+      return res
+        .status(404)
+        .json({ success: false, message: "No blogs found" });
     }
 
     res.status(200).json({ success: true, data: allBlogs });
   } catch (error) {
-    console.error('Error fetching blogs:', error);
-    res.status(500).json({ success: false, message: 'Internal server error' });
+    console.error("Error fetching blogs:", error);
+    res.status(500).json({ success: false, message: "Internal server error" });
   }
 };
 
-//testimonials
 exports.getAllTestimonials = async (req, res) => {
   try {
     const allTestimonials = await Testimonials.find();
 
     if (allTestimonials.length === 0) {
-      return res.status(404).json({ success: false, message: 'No testimonials found' });
+      return res
+        .status(404)
+        .json({ success: false, message: "No testimonials found" });
     }
 
     res.status(200).json({ success: true, data: allTestimonials });
   } catch (error) {
-    console.error('Error fetching blogs:', error);
-    res.status(500).json({ success: false, message: 'Internal server error' });
+    console.error("Error fetching testimonials:", error);
+    res.status(500).json({ success: false, message: "Internal server error" });
   }
 };
